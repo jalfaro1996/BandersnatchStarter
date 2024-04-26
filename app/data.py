b3147f1eb15199697fe0b0bb2c4ddc22526571a2
jalfaro1996 <james.alfaro1996@gmail.com>
2024-04-26 02:48:53 -0700

    BloomTech labs: ticket 2 Data Science

--- app/data.py
+++ app/data.py
@@ -14,20 +14,25 @@ class Database:
     def __init__(self, collection="Monsters"):
         self.collection = self.database.get_collection(collection)
 
-    def seed(self, amount=1000) -> bool:    # correctly inserts the specified number of documents into the collection.
+    def seed(self, amount=1000) -> bool:
+        # correctly inserts the specified number of documents into the collection.
         records = [Monster().to_dict() for i in range(amount)]
         return self.collection.insert_many(records).acknowledged
 
-    def reset(self) -> bool:                # correctly deletes all documents from the collection.
+    def reset(self) -> bool:
+        # correctly deletes all documents from the collection.
         return self.collection.delete_many({}).acknowledged
 
-    def count(self) -> int:                 # correctly returns the number of documents in the collection.
+    def count(self) -> int:                 
+        # correctly returns the number of documents in the collection.
         return self.collection.count_documents({})
 
-    def dataframe(self) -> DataFrame:       # correctly returns a DataFrame containing all documents in the collection.
+    def dataframe(self) -> DataFrame:
+        # correctly returns a DataFrame containing all documents in the collection.
         return DataFrame(list(self.collection.find({}, {"_id": False})))
 
-    def html_table(self) -> str:            #correctly returns an HTML table representation of the DataFrame, or None if the collection is empty.
+    def html_table(self) -> str:
+        #correctly returns an HTML table representation of the DataFrame, or None if the collection is empty.
         count = self.count()
         if count > 0:
             return self.dataframe().to_html()
@@ -36,5 +41,5 @@ class Database:
 
 if __name__ == '__main__':
     db = Database()
-    db.seed()
+    db.count()
     print("collections created:", db.count())
--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,18 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    pass
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    graph = Chart(
+        df,
+        title=f"{y} by {x} for {target}",
+        background="#aaaaaa",
+        width=800,
+        height=800
+    ).mark_circle(size=100).encode(
+        x=x,
+        y=y,
+        color=target,
+        tooltip=Tooltip(df.columns.to_list())
+    )
+    return graph
